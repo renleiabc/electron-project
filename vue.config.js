aeda3b15885f3b683e8b65517acd788741949f2d
renleiabc <421796896@qq.com>
2021-04-12 11:30:13 +0800

    renlei

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@
  * @Author: abc
  * @Date: 2020-10-24 16:13:49
  * @LastEditors: abc
- * @LastEditTime: 2021-04-08 11:17:41
+ * @LastEditTime: 2021-04-12 11:15:36
  * @Description:vue-cil config
  */
 const path = require('path');
@@ -92,7 +92,7 @@ module.exports = {
         mac: {
           icon: './public/img/icons/512.icns'
         },
-        publish: ['github'],
+        publish: [{ provider: 'github' }],
         nsis: {
           oneClick: false,
           perMachine: false,
